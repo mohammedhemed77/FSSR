9489c2b0b35b4b9b7290a8ca7ac6bc3ef395b7b2
Mohammed Hemed <7507590+mohammedhemed77@users.noreply.github.com>
2019-11-19 00:35:20 +0200

    Update main.cpp

--- CS 505/Assignment1/main.cpp
+++ CS 505/Assignment1/main.cpp
@@ -4,46 +4,64 @@ using namespace std;
 class Student
 {
 
-
-public :
-
-    string name ;
-    float grade ;
-
+    public :
+    /* student data */
+    string name ,phone , address ;
+    int code ;
     /* pointer to next Student object */
     Student *next ;
 
     /* setters */
-    void setName(string name) {this->name = name;}
-    void setGrade(float grade){this->grade = grade;}
+    void setName()
+        {
+        cout << "Enter student name : " << endl;
+        cin >>  name ;
+        }
+    void setCode()
+        {
+        cout << "Enter student code : " << endl;
+        cin >>  code ;
+        }
+    void setAddress(){
+        cout << "Enter student address : " << endl;
+        cin >>  address ;
+        }
+    void setphone(){
+        cout << "Enter student phone : " << endl;
+        cin >>  phone ;
+            }
     /* getters */
-    string getName() {  return name;    }
-    float  getGrade(){  return grade;   }
+    string getName()    {  return name;      }
+    string getphone()   {  return phone;     }
+    string getaddress() {  return address;   }
+    int  getCode()      {  return code;      }
     /* Insert new student */
-    void insertStudent (Student **headStudent ,string studentName , float studentGrade , int studentPosition )
+    void insertStudent (Student **headStudent , int studentPosition )
     {
 
      /* cursor is a temp pointer to reach wanted position */
      Student *cursor = *headStudent;
-     /* 
-        create new object of type ( Student ) 
+     /*
+        create new object of type ( Student )
         and return its save its address in a pointer to object Student variable
-        to allow me to deal with this Student node in the (Heap) 
+        to allow me to deal with this Student node in the (Heap)
      */
      Student* newStudent = new Student() ;
-     newStudent->setName (studentName);
-     newStudent->setGrade(studentGrade);
+     newStudent->setName ();
+     newStudent->setAddress();
+     newStudent->setphone();
+     newStudent->setCode();
      newStudent->next = NULL;
 
-     /* if list is empty : */   
+     /* if list is empty : */
      if (studentPosition == 1 )
      {
      *headStudent = newStudent;
      return;
      }
-    /* 
-    to reach specific positoin : 
-    wanted position = (wanted position -1) -> next  
+    /*
+    to reach specific positoin :
+    wanted position = (wanted position -1) -> next
     and this for loop ends before i = wanted position -2
     */
     for (int i = 0 ; i<(studentPosition-2);i++)
@@ -53,7 +71,6 @@ public :
 
         newStudent->next = cursor->next ;
         cursor->next = newStudent ;
-
     }
 
     void printStudentList (Student* headStudent)
@@ -63,8 +80,10 @@ public :
     cout << "-----------------" << endl ;
     while (cursor != NULL)
     {
-        cout <<"student name is " << cursor->name << endl ;
-        cout <<"student grade is " << cursor->grade << endl ;
+        cout <<"student name is "     << cursor->getName()    << endl ;
+        cout <<"student code is "     << cursor->getCode()     << endl ;
+        cout <<"student address is "  << cursor->getaddress() << endl ;
+        cout <<"student phone is "    << cursor->getphone() << endl ;
         cout <<"--------------------" << endl ;
         cursor = cursor->next ;
     }
@@ -76,15 +95,15 @@ public :
 int main()
 {
 
-    Student *head = new Student() ;
-    head->setName("Ali");
-    head->setGrade(50);
-    head->insertStudent(&head,"Alaa    ",150.0,2);
-    head->insertStudent(&head,"Esraa   ",250.0,3);
-    head->insertStudent(&head,"ibrahim ",350.0,4);
-    head->insertStudent(&head,"Nour  ",100.0,5);
-    head->insertStudent(&head,"Dine  ",150.0,6);
-    head->insertStudent(&head,"Aliaa ",350.0,7);
+    Student *head = NULL ;
+
+    head->insertStudent(&head,1);
+    head->insertStudent(&head,2);
+    head->insertStudent(&head,3);
+    head->insertStudent(&head,4);
+    head->insertStudent(&head,5);
+    head->insertStudent(&head,6);
+    head->insertStudent(&head,7);
 
     head->printStudentList(head);
     return 0;
