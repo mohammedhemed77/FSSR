8ec736f20dbdd9cc918eb6b64f6d1bdcd9d76acb
Mohammed Hemed <7507590+mohammedhemed77@users.noreply.github.com>
2019-11-26 14:06:16 +0200

    Update main.cpp

--- CS 505/Linked List/main.cpp
+++ CS 505/Linked List/main.cpp
@@ -2,9 +2,18 @@
 int main()
 {
     List <int ,int > list1;
+       /* insert 10 elements in the list at first */
+    cout << "insert 10 elements in the list at first" << endl;
 
-    list1.deleteFirst();
+    for (int i = 1; i <=10 ; i ++)
+        list1.insertFirst(i,i*10);
+    /* display list elements */
     list1.traverse();
 
+    cout << "insert after element 2 " << endl;
+    list1.insertAfter(2,1000);
+    list1.traverse();
+
+    //list1.insertAfter()
     return 0;
 }
