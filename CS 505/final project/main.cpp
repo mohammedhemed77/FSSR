e29b7d8b1cce9bdaa06221a9fede4f7eface29d0
Mohammed Hemed <7507590+mohammedhemed77@users.noreply.github.com>
2019-12-03 20:19:51 +0200

    Update main.cpp

--- CS 505/final project/main.cpp
+++ CS 505/final project/main.cpp
@@ -3,17 +3,20 @@
 using namespace std ; 
 
 int main() {
-    const int screenWidth = 120 ; 
-       
-    
+    const int screenWidth = 50 ; 
     /* users will send three towers array */
-    int t1[] =  {1,2,3,4,5,6,7,8};
-    int t2[] =  {8,7,6,5,4,3,2,1};
+    /* the worst case of towers is  
+    
+     *   ***    *
+    ***   *    ***             
+    */
     int t3[] =  {1,2,3,4,5,6,7,8};
+    int t2[] =  {8,7,6,5,4,3,2,1};
+    int t1[] =  {1,2,3,4,5,6,7,8};
     
     int diskNumbers = sizeof(t1)/sizeof(int) ;
     cout << " Disk Numbers = " << diskNumbers << endl ;
-    cout << " Minimum Moves = " << (pow(2,diskNumbers) - 1) << endl;  
+    cout << " Minimum Moves = " << (pow(2,diskNumbers) - 1) << endl << endl;  
     
     
     int s1,s2,s3;
@@ -30,20 +33,19 @@ int main() {
     
     (t3[i] % base  == 0) ? s3 = (t3[i]% base): s3 = base - (t3[i] % base);
     
-    if (t1[i]<=t2[i]) s2 -= t1[i] % t2[i];   
-    else s2 += t1[i] % t2[i];
-    if (t2[i]<=t3[i]) s3 -= t2[i] % t3[i];
+    //if (t1[i] != base )  s1 += base / t1[i];
+    if (t2[i] != base )  s2 += t2[i] % base;
+    if (t3[i] != base )  s3 += t3[i] % base;
+    
+    
     int x ; 
-    for (x = 0 ; x < (bigSpace + s1); x++ ) cout << " ";
-    //for (x = 0 ; x < s1 ; x++ ) cout << " ";
+    for (x = 0 ; x < (bigSpace+s1 ); x++ ) cout << " ";
     for (x = 0 ; x < t1[i] ; x++ ) cout << "* ";
     
-    for (x = 0 ; x < (bigSpace + s2 ); x++ ) cout << " ";
-    //for (x = 0 ; x < s2 ; x++ ) cout << " ";
+    for (x = 0 ; x < (bigSpace+s2 ); x++ ) cout << " ";
     for (x = 0 ; x < t2[i] ; x++ ) cout << "* ";
     
-    for (x = 0 ; x < (bigSpace + s3 ); x++ ) cout << " ";
-    //for (x = 0 ; x < s3 ; x++ ) cout << " ";
+    for (x = 0 ; x < (bigSpace +s3); x++ ) cout << " ";
     for (x = 0 ; x < t3[i] ; x++ ) cout << "* ";
     
     for (x = 0 ; x < bigSpace ; x++ ) cout << " ";
@@ -54,20 +56,20 @@ int main() {
         
     return 0;
 }
+      
+/****************************************************************************************************************************** 
+                                                   OUTPUT :
+                                                 ***********   
+    Disk Numbers = 8
+    Minimum Moves = 255
+                                 *                                   * * * * * * * *                                   *                   
+                                * *                                   * * * * * * *                                   * *                   
+                               * * *                                   * * * * * *                                   * * *                   
+                              * * * *                                   * * * * *                                   * * * *                   
+                             * * * * *                                   * * * *                                   * * * * *                   
+                            * * * * * *                                   * * *                                   * * * * * *                   
+                           * * * * * * *                                   * *                                   * * * * * * *                   
+                          * * * * * * * *                                   *                                   * * * * * * * *                   
 
-
-
-/* 
-    s2 = base - (t2[i] % base) ;
-    s3 = base - (t3[i] % base) ;
-    */
-     
-    
-    /*
-    cout << "s1 = " << s1  << endl ; 
-    cout << "s2 = " << s2  << endl ;
-    cout << "s3 = " << s3  << endl ;
-    cout << "bigSpace = " << bigSpace << endl ;
-    */
-    
-    
+    RUN SUCCESSFUL (total time: 1s)
+********************************************************************************************************************************/
