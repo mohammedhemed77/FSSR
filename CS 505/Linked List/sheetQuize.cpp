1299acb1e840e0caa31356ea08a1931d3c373f8b
Mohammed Hemed <7507590+mohammedhemed77@users.noreply.github.com>
2019-12-10 02:21:18 +0200

    Create sheetQuize.cpp

---
+++ CS 505/Linked List/sheetQuize.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std ;
+#include "List.cpp"
+int main()
+{
+    List <int ,int  > list1;
+    List <int ,int  > list2;
+    int element ; 
+ 
+    /* insert 10 elements in the list at first */
+    cout << "insert 10 elements in the list at first" << endl;
+    for (int i = 1; i <=10 ; i ++)
+    list1.insertFirst(i,i);
+    
+    /* display list elements */
+    list1.traverse();
+    
+    list1.toFirst();
+    list2.toFirst();
+    while (!list1.atEnd())
+    {
+     list1.retrieveData(element);
+     /* if element is odd */
+     if (element %2 != 0) list2.insertAfter(2,element); 
+     list1.advance();
+    }
+    list1.toFirst();
+    while (!list1.atEnd())
+    {
+     list1.retrieveData(element);
+     /* if element is even */
+     if (element %2 == 0) list2.insertAfter(2,element);
+     list1.advance();
+    }
+    /* display list 2 after edit the elements */
+    list2.traverse();
+    
+    
+    return 0;
+}
